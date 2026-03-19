e26452450ca3ee860094e1cb258925e40ff33884
Sandy Dunlop <sandy.dunlop@gmail.com>
2026-03-19 09:03:20 +0000

    Init

---
+++ src/main/java/io/github/qishr/cascara/macos/menus/package-info.java
@@ -0,0 +1,1 @@
+package io.github.qishr.cascara.macos.menus;
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module cascara.macos.menus {
+    requires javafx.base;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
+
+    requires nsmenufx;
+    requires cascara.ui;
+
+    provides io.github.qishr.cascara.ui.platform.SystemMenusService
+        with io.github.qishr.cascara.macos.menus.MacosSystemMenus;
+
+}
